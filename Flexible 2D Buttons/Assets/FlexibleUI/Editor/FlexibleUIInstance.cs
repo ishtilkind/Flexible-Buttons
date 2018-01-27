f5bea21cdc80788fd7bf942c816d460a0ed08a76
ishtilkind <github@shtilkind.net>
2018-01-27 12:10:20 -0800

    Editor script added

---
+++ Flexible 2D Buttons/Assets/FlexibleUI/Editor/FlexibleUIInstance.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class FlexibleUIInstance : Editor {
+
+    static GameObject clickedObject;
+
+    private static GameObject Create (string objectName)
+    {
+        GameObject instance = Instantiate(Resources.Load<GameObject>(objectName));
+        instance.name = objectName;
+        clickedObject = UnityEditor.Selection.activeObject as GameObject;
+        if( null != clickedObject)
+        {
+            instance.transform.SetParent(clickedObject.transform, false);
+        }
+
+        return instance;
+    }
+}
